8e4669597060a514509e1845a610e76b7407a83b
Alexander Medved <medved1991alexander@gmail.com>
2024-03-07 15:14:00 +0300

    Create models

---
+++ src/LinkBook.Services.UrlAPI/Models/Dto/LinkDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkBook.Services.UrlAPI.Models
+{
+    public class LinkDto
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string AliasUrl{ get; set; }
+        public string OriginalUrl { get; set; }
+        public string Tag { get; set; }
+    }
+}
---
+++ src/LinkBook.Services.UrlAPI/Models/Link.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkBook.Services.UrlAPI.Models
+{
+    public class Link
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public string AliasUrl{ get; set; }
+        public string OriginalUrl { get; set; }
+        public string Tag { get; set; }
+    }
+}
