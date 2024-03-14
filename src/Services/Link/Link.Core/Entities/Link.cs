9061f6c59fb4046bf92d71156859a0aaa468d8d6
Alexander Medved <medved1991alexander@gmail.com>
2024-03-14 15:41:26 +0300

    Add repository interface

---
+++ src/Services/Link/Link.Core/Entities/Link.cs
@@ -0,0 +1,10 @@
+﻿namespace Link.Core.Entities;
+
+public sealed class Link : BaseEntity
+{
+    public string UserId { get; set; }
+    public string AliasUrl{ get; set; }
+    public string OriginalUrl { get; set; }
+    public string Tag { get; set; }
+    public bool Favorite { get; set; }
+}
---
+++ src/Services/Link/Link.Core/Interfaces/ILinkRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Link.Core.Interfaces;
+using Link = Entities.Link;
+
+public interface ILinkRepository
+{
+    Task<IEnumerable<Link>> GetAllForUser(string userId);
+    Task<IEnumerable<Link>> GetFavoritesForUser(string userId);
+    Task<Link> GetById(Guid id);
+    Task<bool> Update(Link link);
+    Task<bool> Delete(Guid id);
+    Task<Link> Create(Link link);
+}
