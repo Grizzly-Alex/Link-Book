41fc7e2d0ff8649d94c5ed7b82a609fa610c66c3
Alexander Medved <medved1991alexander@gmail.com>
2024-12-02 14:27:58 +0300

    command abstractions

---
+++ src/Services/Link/Link.Application/Commands/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Link.Core.Entities;
+using MediatR;
+
+namespace Link.Application.Commands;
+
+public interface ICommandHandler<TCommand> :  IRequestHandler<TCommand, Result>
+    where TCommand : ICommand
+{
+}
+
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+}
