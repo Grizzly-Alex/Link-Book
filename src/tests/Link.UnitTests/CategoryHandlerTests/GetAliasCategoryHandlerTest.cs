bff19977c5b186e8eb32e1325da5b7f2a1fbed9f
Alexander Medved <medved1991alexander@gmail.com>
2025-01-20 16:29:32 +0300

    mend

--- src/tests/Link.UnitTests/CategoryHandlerTests/GetAliasCategoryHandlerTest.cs
+++ src/tests/Link.UnitTests/CategoryHandlerTests/GetAliasCategoryHandlerTest.cs
@@ -8,6 +8,7 @@ using Link.Core.Entities.Category;
 using Link.Application.Utilities;
 using Link.Application.Queries.AliasCategoryQueries;
 using System.Linq.Expressions;
+using Link.Core.Entities;
 
 
 namespace Link.UnitTests.CategoryHandlerTests;
@@ -49,5 +50,33 @@ public class GetAliasCategoryHandlerTest
         result.IsSuccess.Should().BeFalse();
         result.IsFailure.Should().BeTrue();
         result.Error.Should().Be(CategoryErrors.NotFound);
+    }
+
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult_WhenUserHaveSomeCategories()
+    {
+        // Arrange
+        var query = new GetAllAliasCategoriesByUserQuery(string.Empty);
+
+        _categoryRepositoryMock.Setup(
+            x => x.GetAll(
+                It.IsAny<Expression<Func<AliasCategory, bool>>>(),
+                It.IsAny<Func<IQueryable<AliasCategory>, IOrderedQueryable<AliasCategory>>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<AliasCategory>() { new AliasCategory(string.Empty, string.Empty)});
+
+        var handler = new GetAllAliasCategoriesByUserHandler(
+                _categoryRepositoryMock.Object,
+                _mapper);
+
+        // Act
+        var result = await handler.Handle(query, default);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.IsFailure.Should().BeFalse();
+        result.Error.Should().Be(Error.None);
+        result.Value.Should().NotBeNull();
     }
 }
